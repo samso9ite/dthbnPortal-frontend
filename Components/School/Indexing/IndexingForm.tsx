fd4f208c68d950f7699a668e9f5e4fda0a0366f7
Samson Ajayi <samso9ite@gmail.com>
2023-11-17 18:22:26 +0100

    Forms

--- Components/School/Indexing/IndexingForm.tsx
+++ Components/School/Indexing/IndexingForm.tsx
@@ -4,10 +4,14 @@ import { useCustomMutation } from "@/Hooks/apiCall"
 import GenericForm, {FormValues} from "@/UI/genericForm"
 import { indexingActions, stepperState } from "@/store/indexing-slice"
 import { useDispatch, useSelector } from "react-redux"
+import {useState} from 'react'
 
 const IndexingForm = () => {
     const dispatch = useDispatch()
-    let formState = useSelector(stepperState) 
+    let formState = useSelector(stepperState)
+    const[referees, setReferees] = useState([
+        {'referee_name': '', 'referee_address': '', 'referee_number': ''}
+    ]) 
 
     const onSuccess = () => {
          
@@ -24,15 +28,35 @@ const IndexingForm = () => {
         }
     }
 
-    return(
+    const addReferee = () => {
+        console.log("This worked");
+        
+        setReferees([...referees, {'referee_name': '', 'referee_address': '', 'referee_number': ''}])
+        console.log(referees);
+        
+    }
+
+    return( 
         <>
-            <GenericForm fields={ formState == 'profile' ? Fields.indexingProfileFields : 
+            <GenericForm fields= { formState == 'profile' ? Fields.indexingProfileFields : 
                     formState == 'work' ? Fields.indexingWorkFields :
-                    formState == 'referee' ?  Fields.indexingRefereeFields :
+                    formState == 'referee' ?  referees.map((referee, index) => [
+                        { name: `referee_name_${index}`, type: 'text', label: 'Referee Name', required: true },
+                        { name: `referee_number_${index}`, type: 'number', label: 'Referee Number', required: true },
+                        { name: `referee_address_${index}`, type: 'text', label: 'Referee Address', required: true },
+                      ]) :
                     Fields.indexingResultFields
+                    // formState == 'work' ? Fields.indexingWorkFields :
+                    // formState == 'referee' ? Fields.indexingRefereeFields :
+                    // Fields.indexingResultFields
                 } 
                 onSubmit={submitHandler} span6={true} stepperForm={true} 
             />
+            {formState === 'referee' && (
+                <button type="button" onClick={addReferee}>
+                Add Referee
+                </button>
+            )}
         </>
     )
 }
