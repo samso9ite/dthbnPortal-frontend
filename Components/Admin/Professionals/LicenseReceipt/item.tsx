5909a5008bb57463fc3c842e98e48983a9ed3aa7
Samso9ite <samso9ite@gmail.com>
2024-07-09 21:45:35 +0100

    Upload receipt

--- Components/Admin/Professionals/LicenseReceipt/item.tsx
+++ Components/Admin/Professionals/LicenseReceipt/item.tsx
@@ -6,8 +6,6 @@ import Link from 'next/link';
 
 const ReceiptItem = (props:any) => {
     const [modalIsOpen, setIsModalOpen] = useState<boolean>(false)
-    console.log(props);
-    
     
     return(
         <>
